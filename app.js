62dd2cc9708b4b1251496fc463a78fcad0b590fd
matt Klein <mwklein212@gmail.com>
2022-06-21 13:08:30 -0400

    completed asignment

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
+
+let promise = new Promise((resolve, reject) => {
+  let userLeft = Boolean(Math.round(Math.random()));
+
+   if (userLeft) {
+     reject("User Left.");
+   } else {
+     resolve("Thumbs up and Subscribe!");
+   }
+});
+
+promise
+  .then((result) => {
+  }).catch((err) => {})
+
+    
+
+
+
+
+function watchTurorialCallback(callback, errorCallback) {
+    let userLeft = false;
+  
+    if (userLeft) {
+      errorCallback("User left.");
+    } else {
+      callback("Thumbs up and Subscribe!");
+    }
+  }
+  
+  watchTurorialCallback(
+    (message) => {
+      console.log(message);
+    },
+    (error) => {
+      console.log(error.name + " " + error.message);
+    }
+  );
+
 console.log("EXERCISE 1:\n==========\n");
